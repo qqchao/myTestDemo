8621e7aa43c772930c5b1a4fa7bea8a944ff4b6f
qqchao <qc9201@sina.com>
2017-12-11 11:33:08 +0800

    终于成功转mavenWeb了

---
+++ src/main/java/com/cf/demo/list/remove/TestRemove.java
@@ -0,0 +1,22 @@
+package com.cf.demo.list.remove;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestRemove {
+
+	public static void main(String[] args) {
+        String[] s={"1","2","2","3","4","5","6"};
+        List<String> listA = Arrays.asList(s);
+        List<String> listB = new ArrayList<>(listA);
+        for(int i=0;i<listB.size();i++){
+            if (listB.get(i).equals("2")){
+                listB.remove(i);
+            }
+        }
+        for(String temp:listB){
+            System.out.println(temp);
+        }
+    }
+}
